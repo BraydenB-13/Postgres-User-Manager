a1c0a6f848c1960982b7afc59e6978317ed98654
Brayden Barger <brayden.barger8273@stu.mtec.edu>
2021-03-18 10:01:37 -0600

    fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const db = require('./db');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
