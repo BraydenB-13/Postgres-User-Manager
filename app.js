ab58e865097b5627bcde8e4ee23e53c1594b3e8d
Brayden Barger <brayden.barger8273@stu.mtec.edu>
2021-03-18 09:35:20 -0600

    fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Pool = require('pg').Pool;
 require('dotenv').config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const app = express();
 const path = require('path');
